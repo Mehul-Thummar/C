600e0e2da3e7247ebf16680d0af60590eabbdb15
BBS CREDIT CO-OP. SOC. LTD <bbsofcbackup@gmail.com>
2023-10-04 15:12:08 +0530

    name chnage

---
+++ class_pro/constructors.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+class person
+{
+private:
+    int age;
+    string city, name;
+
+    public:
+    person()
+    {
+        cout<<"Enter the Name:";
+        cin>>name;
+        cout<<"Enter the City:";
+        cin>>city;
+        cout<<"Enter the Age:";
+        cin>>age;
+
+    }
+     void setname(string n)
+    {
+        name= n ;
+    }
+    void setcity(string c)
+    {
+        city = c;
+    }
+    void setage(int a)
+    {
+        age = a;
+    }
+
+    void getname()
+    {
+        cout <<"PERSON NAME:" << name << endl;
+    }
+    void getcity()
+    {
+        cout <<"PERSON CITY:" << city << endl;
+    }
+    void getage()
+    {
+        cout <<"PERSON AGE:" << age << endl;
+    }
+
+};
+
+int main()
+{
+    person p1;
+    person p2;
+
+return 0;
+}
