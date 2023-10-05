52baf0dec7520c735b56db164605acf11fe83900
BBS CREDIT CO-OP. SOC. LTD <bbsofcbackup@gmail.com>
2023-10-05 16:41:54 +0530

    add new

---
+++ constructar/destructor.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class arith
+{
+    int a, b, t, total;
+
+public:
+    arith()
+    {
+
+        cout << "Enter A value: ";
+        cin >> a;
+        cout << "Enter B value: ";
+        cin >> b;
+    }
+    ~arith()
+    {
+
+        cout<<".............................";
+    }
+    void settotal()
+    {
+        t = a + b;
+        total = t;
+    }
+    void gettotal()
+    {
+        cout << "TOTAL: " << t << endl;
+    }
+};
+
+int main()
+{
+    arith a1;
+    a1.settotal();
+    a1.gettotal();
+
+    arith a2;
+
+    return 0;
+}
