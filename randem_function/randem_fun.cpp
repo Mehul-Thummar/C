fb7244c95a9a014b74cdca3b856d347f2b0e9bbd
BBS CREDIT CO-OP. SOC. LTD <bbsofcbackup@gmail.com>
2023-11-01 16:56:32 +0530

    add program

--- randem_function/randem_fun.cpp
+++ randem_function/randem_fun.cpp
@@ -5,8 +5,12 @@ using namespace std;
 
 int main()
 {
+    int max = 99999;
+    int min = 55555;
     int x = rand();
 
+
+    x = min + rand() + (max - min + 1);
     cout << "Randam Number is : " << x;
 
     return 0;
